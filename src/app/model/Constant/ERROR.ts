50311f91873651a175fd24c447e3b503a6cef8a4
ahmedJamaal <ahmed.jamaal93@gmail.com>
2024-11-06 00:36:46 +0200

    Auto-generated commit

--- src/app/model/Constant/ERROR.ts
+++
@@ -1,8 +0,0 @@
-export const ERROR: any[] = [
-
- {id:404 ,title:'404_title', message:'404_message'},
- {id:401 ,title:'401_title', message:'401_message'},
- {id:403 ,title:'403_title', message:'403_message'},
- {id:500 ,title:'500_title', message:'500_message'},
- {id:502 ,title:'502_title', message:'502_message'}
-];
--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://sadeem.eastus.cloudapp.azure.com:7070/CRCICA/Api',
-  App_Name: 'Abydos_Stone',
-  currentUser: `Abydos-Stone-$User`,
-  CurrentLang: `Abydos-Stone-$Lang`,
-
-
-};
